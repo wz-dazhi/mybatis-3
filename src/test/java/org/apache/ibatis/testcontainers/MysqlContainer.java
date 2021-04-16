cb554447dd590b8c6319261f19aba50a2447e84c
wz-dazhi <742820195@qq.com>
2021-04-16 21:08:51 +0800

    update

--- src/test/java/org/apache/ibatis/testcontainers/MysqlContainer.java
+++ src/test/java/org/apache/ibatis/testcontainers/MysqlContainer.java
@@ -1,24 +1,24 @@
 /**
- *    Copyright 2009-2021 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.testcontainers;
 
-import javax.sql.DataSource;
-
 import org.apache.ibatis.datasource.unpooled.UnpooledDataSource;
 import org.testcontainers.containers.MySQLContainer;
+
+import javax.sql.DataSource;
 
 public class MysqlContainer {
 
@@ -31,15 +31,16 @@ public class MysqlContainer {
 
   private static MySQLContainer<?> initContainer() {
     @SuppressWarnings("resource")
-    MySQLContainer<?> container = new MySQLContainer<>().withDatabaseName(DB_NAME).withUsername(USERNAME)
-        .withPassword(PASSWORD).withUrlParam("useSSL", "false");
+    MySQLContainer<?> container = new MySQLContainer<>()
+      .withDatabaseName(DB_NAME)
+      .withUsername(USERNAME)
+      .withPassword(PASSWORD);
     container.start();
     return container;
   }
 
   public static DataSource getUnpooledDataSource() {
-    return new UnpooledDataSource(MysqlContainer.DRIVER, INSTANCE.getJdbcUrl(), MysqlContainer.USERNAME,
-        MysqlContainer.PASSWORD);
+    return new UnpooledDataSource(MysqlContainer.DRIVER, INSTANCE.getJdbcUrl(), MysqlContainer.USERNAME, MysqlContainer.PASSWORD);
   }
 
   private MysqlContainer() {
